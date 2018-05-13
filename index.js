1ac3fcb230b12a2b79365db920cc5e72b3363ea3
kathrin v <go.kathrin@gmail.com>
2018-05-13 22:41:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function 
